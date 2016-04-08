ac36c47a7b46a40665aa402f23390b782c6e4c3a
mansenfranzen <franz.woellert@gmail.com>
2016-04-08 18:35:10 +0200

    Changed directory structure to standard python distribution. Included setup.py. Moved docs outside of main package. Renamed main package to mlearn.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath('../../'))
+import mlearn
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -119,7 +120,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = 'alabaster'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
--- docs/source/linear_regression.rst
+++ docs/source/linear_regression.rst
@@ -3,5 +3,6 @@ Linear Regression
 
 Documentation for the linear regression module.
 
-.. autofunction:: linear_regression.sum_of_squared_residuals
-.. autofunction:: linear_regression.ordinary_least_squares
+.. autofunction:: mlearn.linear_regression.sum_of_squared_residuals
+.. autofunction:: mlearn.linear_regression.gradient_descent
+.. autofunction:: mlearn.linear_regression.ordinary_least_squares
